d623b05fe2ac5de26a5a7d83055e60cf1d56f161
Felipe Miranda <lipemiranda159@hotmail.com>
2020-08-06 01:18:45 -0300

    remove unused references

--- FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
+++ FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
@@ -3,7 +3,6 @@ using WebAtividadeEntrevista.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using FI.AtividadeEntrevista.DML;
 
