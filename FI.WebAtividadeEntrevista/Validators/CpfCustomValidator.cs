3614c91c984e950303b0475175f974fc0ce2a99b
Felipe Miranda <lipemiranda159@hotmail.com>
2020-08-06 04:28:00 -0300

    Create custom validator for cpf

---
+++ FI.WebAtividadeEntrevista/Validators/CpfCustomValidator.cs
@@ -0,0 +1,22 @@
+﻿using FI.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Validators
+{
+    public class CpfCustomValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            
+            if (!CpfValidator.IsCpf(value.ToString()))
+            {
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+            return null;
+        }
+    }
+}
